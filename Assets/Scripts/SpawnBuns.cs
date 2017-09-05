973a12fa10ffb5bf9c2059584338258733da1a42
John Mark Adriano <thearchunterx@gmail.com>
2017-09-05 09:11:41 +0400

    Stuff

---
+++ Assets/Scripts/SpawnBuns.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZM.JM.SubSystem
+{
+    public class SpawnBuns : MonoBehaviour
+    {
+        public GameObject topBunS,botBunS, topBunPrefab, botBunPrefab;
+        public void Respawn()
+        {
+            //ntiate(botBunPrefab, botBunS);
+        }
+    }
+}
